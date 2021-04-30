bcecd7772e8e12f45eed3876e06510948aa1b49c
moali99 <mohammadali.mirzaee99@gmail.com>
2021-04-30 10:55:47 +0200

    database

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const express = require('express')
 const lowdb = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
 
 const app = express();
+/* app.get('/', (req, res) => {
+}) */
+
+const adapter = new FileSync('menu-json')
+const database = lowdb(adapter);
+
+app.use(express.json());
+
+database.defaults({ menu: [] }).write();
 
 
-app.get('/', (req, res) => {
-    
-    res.send("Hello")
+app.get('/menu', (request, response) => {
+    const menu = request.body;
+    response.json(menu)
 })
 
 app.listen(5000, () => {
