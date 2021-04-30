80613a65fd9fe169aada5268c4c97c34b219d19a
Ivar Olsson <ivar.olsson2323@gmail.com>
2021-04-30 13:29:39 +0200

    Lägga till accounts

--- server.js
+++ server.js
@@ -22,5 +22,48 @@ app.get('/menu', (request, response) => {
 })
 
 app.listen(5000, () => {
-    console.log("sever started")
+    console.log("server started")
+    initiateDatabase();
 })
+
+
+
+
+
+
+
+// skapa accounts
+const adapter = new FileSync('accounts.json');
+const database = lowdb(adapter);
+
+function initiateDatabase() {
+    database.defaults({ accounts: [] }).write();
+};
+
+
+app.post('/api/signup', (request, response) => {
+   const account = request.body;
+   console.log('Konto att lägga till:', account);  
+}) 
+
+const account = {
+    username: 'Test',
+    email: 'test@test.se',
+    password: '12345'
+}
+
+// database.get('accounts').push(account).write();
+
+const accounts = database.get('accounts').value();
+console.log('account-databasen innhåller:', JSON.stringify(accounts));
+
+
+
+// Fetch-anrop för eventuell frontend-sida
+// fetch('http://localhost:5000/api/signup', {
+//     method: 'POST',
+//     headers: {
+//         'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify(account)
+// })
